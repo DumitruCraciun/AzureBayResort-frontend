547a3652ddc22fcb404b7b1bfffd0633eaf7de60
Dumitru Craciun <mediainfo_on@yahoo.com>
2026-08-28 13:11:02 +0100

    Add route for booking-confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ function App() {
               <Route path="/rooms/:id" element={<RoomDetails />} />
               <Route path="/guest-checkout" element={<GuestCheckout />} />              
               <Route path="/bookingconfirmation/:id" element={<BookingConfirmation />} />
+              <Route path="/booking-confirmation/:id" element={<BookingConfirmation />} /> {/* backup */}
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/booking/:id" element={
